48b627ee6b7c5a74ef8a59f119e15b756396b957
Cenk Alti <cenkalti@gmail.com>
2020-08-27 17:48:22 +0300

    fix linter warnings

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
-import { app, Menu, Tray, net } from 'electron';
-import * as path from 'path';
-import { spawn } from 'child_process';
-import * as getPort from 'get-port';
+import { app, Menu, Tray, net } from 'electron'
+import * as path from 'path'
+import { spawn } from 'child_process'
+import * as getPort from 'get-port'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -9,43 +9,42 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 }
 
 declare var __static: string
-console.log("__static", __static)
 const iconPath = path.join(__static, 'img', 'IconTemplate.png')
 const host = '127.0.0.1'
 
-function createMenu(syncStatus: string) {
+function createMenu (syncStatus: string) {
   return Menu.buildFromTemplate([
-    {label: syncStatus, id: 'syncStatus', enabled: false},
-    {label: 'Quit', role: 'quit'},
+    { label: syncStatus, id: 'syncStatus', enabled: false },
+    { label: 'Quit', role: 'quit' }
   ])
 }
 
 app.on('ready', () => {
   const tray = new Tray(iconPath)
   tray.setContextMenu(createMenu('Starting to sync...'))
-  getPort({host: host}).then(port => {
-    const ls = spawn(path.join(__static, 'bin', 'putio-sync'), ['-repeat', '10s', '-server', host + ':' + port], {stdio: ['ignore', 'ignore', 'pipe']});
-    ls.stderr.on('data', (data) => {console.log(String(data).trim())});
-    ls.on('close', (code) => {console.log(`child process exited with code ${code}`);});
+  getPort({ host: host }).then(port => {
+    const ls = spawn(path.join(__static, 'bin', 'putio-sync'), ['-repeat', '10s', '-server', host + ':' + port], { stdio: ['ignore', 'ignore', 'pipe'] })
+    ls.stderr.on('data', (data) => { console.log(String(data).trim()) })
+    ls.on('close', (code) => { console.log(`child process exited with code ${code}`) })
     setInterval(() => {
       const statusURL = `http://${host}:${port}/status`
       const request = net.request(statusURL)
       request.on('response', (response) => {
-        if (response.statusCode != 200) {
+        if (response.statusCode !== 200) {
           return
         }
         response.on('data', (chunk) => {
           let parsed = null
           try {
             parsed = JSON.parse(String(chunk))
-          } catch(e) {
+          } catch (e) {
             return
           }
           tray.setContextMenu(createMenu(parsed.status))
         })
       }).on('error', (error) => {
         console.log(`ERROR: ${error}`)
-      }).end();
-    }, 1000);
+      }).end()
+    }, 1000)
   })
 })
