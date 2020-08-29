81195e4c07baa650eb17cd21873e883020c6640e
Cenk Alti <cenkalti@gmail.com>
2020-08-29 12:34:48 +0300

    download binary based on platform

--- downloadBinary.ts
+++ downloadBinary.ts
@@ -1,9 +1,11 @@
-const path = require('path')
+const os = require('os')
 const fs = require('fs')
+const path = require('path')
 const axios = require('axios')
 const util = require('util')
 const zlib = require('zlib')
 const tar = require('tar-stream')
+const unzipper = require('unzipper')
 
 async function getLatestBinaryVersion (repo: string): Promise<string> {
   const releasesURL = `https://api.github.com/repos/${repo}/releases`
@@ -14,13 +16,11 @@ async function getLatestBinaryVersion (repo: string): Promise<string> {
 async function downloadFileFromTarGZ (url: string, dest: string, filename: string) {
   const writer = fs.createWriteStream(dest)
   const extract = tar.extract()
-  let found = false
   extract.on('entry', function (header: any, stream: any, next: Function) {
     if (header.name !== filename) {
       next()
       return
     }
-    found = true
     writer.on('finish', next)
     stream.pipe(writer)
   })
@@ -28,22 +28,49 @@ async function downloadFileFromTarGZ (url: string, dest: string, filename: strin
   response.data.pipe(zlib.createGunzip()).pipe(extract)
   return new Promise((resolve, reject) => {
     writer.on('finish', resolve)
-    extract.on('finish', () => {
-      if (!found) {
-        reject(new Error('file not found in archive'))
-      }
-    })
+    extract.on('finish', reject)
+  })
+}
+
+async function downloadFileFromZip (url: string, dest: string, filename: string) {
+  const writer = fs.createWriteStream(dest)
+  const extract = unzipper.Parse()
+  extract.on('entry', function (entry: any) {
+    if (entry.path !== filename) {
+      entry.autodrain()
+      return
+    }
+    writer.on('finish', entry.autodrain)
+    entry.pipe(writer)
+  })
+  const response = await axios({ url, method: 'GET', responseType: 'stream' })
+  response.data.pipe(extract)
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    extract.on('finish', reject)
   })
 }
 
 (async () => {
   const mkdir = util.promisify(fs.mkdir)
-  await mkdir(path.join('static', 'bin'), { recursive: true })
-  const binPath = path.join('static', 'bin', 'putio-sync')
-  const binaryRepo = 'putdotio/putio-sync'
-  const putioSyncVersion = await getLatestBinaryVersion(binaryRepo)
-  const putioSyncURL = `https://github.com/putdotio/putio-sync/releases/download/v${putioSyncVersion}/putio-sync_${putioSyncVersion}_macos_x86_64.tar.gz`
-  await downloadFileFromTarGZ(putioSyncURL, binPath, 'putio-sync')
-  const chmod = util.promisify(fs.chmod)
-  await chmod(binPath, 0o775)
+  const binPath = path.join('static', 'bin')
+  await mkdir(binPath, { recursive: true })
+  const repo = 'putdotio/putio-sync'
+  const version = await getLatestBinaryVersion(repo)
+  switch (os.platform()) {
+    case 'darwin': {
+      const url = `https://github.com/${repo}/releases/download/v${version}/putio-sync_${version}_macos_x86_64.tar.gz`
+      const target = path.join(binPath, 'putio-sync')
+      await downloadFileFromTarGZ(url, target, 'putio-sync')
+      const chmod = util.promisify(fs.chmod)
+      await chmod(target, 0o775)
+      break
+    }
+    case 'win32': {
+      const url = `https://github.com/${repo}/releases/download/v${version}/putio-sync_${version}_windows_x86_64.zip`
+      const target = path.join(binPath, 'putio-sync.exe')
+      await downloadFileFromZip(url, target, 'putio-sync.exe')
+      break
+    }
+  }
 })()
