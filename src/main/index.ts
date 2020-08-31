4972ae2460557d8b7db5ada2b142da26dae4bff1
Cenk Alti <cenkalti@gmail.com>
2020-08-31 13:14:56 +0300

    show config on startup

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,10 @@
-import { app, Menu, Tray, net, shell } from 'electron'
+import { app, Menu, Tray, net, shell, dialog } from 'electron'
 import * as path from 'path'
 import { spawn, spawnSync } from 'child_process'
 import * as getPort from 'get-port'
 import * as log from 'electron-log'
 import * as os from 'os'
+import * as fs from 'fs'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -15,15 +16,21 @@ const iconPath = path.join(__static, 'img', 'IconTemplate.png')
 const host = '127.0.0.1'
 const logPath = log.transports.file.getFile().path
 const binPath = path.join(__static, 'bin')
+const exitCodeConfigError = 10
 var exe = 'putio-sync'
 if (os.platform() === 'win32') {
   exe += '.exe'
 }
+const configPath = String(spawnSync(path.join(binPath, exe), ['-print-config-path']).stdout).trim()
 
-function openConfig () {
-  const child = spawnSync(path.join(binPath, exe), ['-print-config-path'])
-  const configPath = String(child.stdout).trim()
-  shell.openPath(configPath)
+async function openConfig () {
+  fs.closeSync(fs.openSync(configPath, 'a'))
+  await shell.openPath(configPath)
+}
+
+function restartApp () {
+  app.relaunch()
+  app.exit()
 }
 
 function createMenu (syncStatus: string) {
@@ -31,7 +38,7 @@ function createMenu (syncStatus: string) {
     { label: syncStatus, id: 'syncStatus', enabled: false },
     { label: 'Open config file', click: openConfig },
     { label: 'Open log file', click: () => { shell.openPath(logPath) } },
-    { label: 'Restart', click: () => { app.relaunch(); app.exit() } },
+    { label: 'Restart', click: restartApp },
     { label: 'Quit', role: 'quit' }
   ])
 }
@@ -40,9 +47,27 @@ app.on('ready', () => {
   const tray = new Tray(iconPath)
   tray.setContextMenu(createMenu('Starting to sync...'))
   getPort({ host: host }).then(port => {
-    const child = spawn(path.join(binPath, exe), ['-repeat', '1m', '-server', host + ':' + port], { stdio: ['ignore', 'ignore', 'pipe'] })
+    const child = spawn(path.join(binPath, exe), ['-repeat', '1m', '-server', host + ':' + port], { stdio: ['ignore', 'pipe', 'pipe'] })
+    var stdout = ''
+    child.stdout.on('data', (data) => { stdout += String(data) })
     child.stderr.on('data', (data) => { log.info(String(data).trim()) })
-    child.on('close', (code) => { log.error(`child process exited with code ${code}`) })
+    child.on('close', (code) => {
+      log.error(`child process exited with code ${code}`)
+      if (code === exitCodeConfigError) {
+        if (!fs.existsSync(configPath)) {
+          // Put default config file with some placeholders.
+          fs.writeFileSync(configPath, 'username = ""\npassword = ""\n')
+        }
+        dialog.showMessageBoxSync({
+          type: 'error',
+          title: 'Error in config',
+          message: 'Error in config: ' + stdout.trim(),
+          detail: 'Press OK to edit config file. Relaunch the app after editing config.'
+        })
+        openConfig()
+        app.quit()
+      }
+    })
     setInterval(() => {
       const statusURL = `http://${host}:${port}/status`
       const request = net.request(statusURL)
@@ -59,9 +84,11 @@ app.on('ready', () => {
           }
           tray.setContextMenu(createMenu(parsed.status))
         })
-      }).on('error', (error) => {
+      })
+      request.on('error', (error) => {
         log.error(`ERROR: ${error}`)
-      }).end()
+      })
+      request.end()
     }, 1000)
   })
 })
